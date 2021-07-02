f7241ef11e976c27286c56e8363a11dfdc3f8ba3
Quoc Huy <pqhuy0211@gmail.com>
2021-07-02 11:49:44 +0700

    Update

--- frontend/src/components/Banner/Banner.tsx
+++ frontend/src/components/Banner/Banner.tsx
@@ -4,7 +4,7 @@ import "./Banner.scss";
 export const Banner: React.FC = () => {
   return (
     <div className="banner-container">
-      <div></div>
+      <div>Welcome to MyTutor</div>
     </div>
   );
 };
