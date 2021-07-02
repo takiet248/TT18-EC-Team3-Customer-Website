c6ec4abc0ef7b1fe55a58d6bfb3729a82f553eb5
Quoc Huy <pqhuy0211@gmail.com>
2021-07-02 23:13:42 +0700

    update banner component

--- frontend/src/components/Banner/Banner.tsx
+++ frontend/src/components/Banner/Banner.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./Banner.scss";
+import { Button } from "../common";
 
 export const Banner: React.FC = () => {
   return (
     <div className="banner-container">
-      <div>Welcome to MyTutor</div>
+      <div className="banner-overlay"></div>
+      <div className="banner-content">
+        <h1>Solving learning problems is easier than ever!</h1>
+        <p>Learn the subjects you need with top tutors on our platform.</p>
+        <div className="banner-button">
+          <Button width={150} height={50} fontSize={20}>
+            Sign Up
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
