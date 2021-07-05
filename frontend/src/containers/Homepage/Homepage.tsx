aeff0a426871ebf5329e45adccf34ea834f6ebfc
Quoc Huy <pqhuy0211@gmail.com>
2021-07-05 17:04:03 +0700

    introduction component

--- frontend/src/containers/Homepage/Homepage.tsx
+++ frontend/src/containers/Homepage/Homepage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Banner, TutorsList } from "../../components";
+import { Banner, TutorsList, Introduction } from "../../components";
 
 export const Homepage: React.FC = () => {
   return (
     <div>
       <Banner />
       <TutorsList />
+      <Introduction />
     </div>
   );
 };
