c8747fe9734a55bbe0d615fa955512c13020837e
Trần Anh Kiệt <anhkiett524820@gmail.com>
2021-07-20 23:02:54 +0700

    delete backend

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "port" : process.env.PORT,
-    "db" : process.env.DB_CONNECT,
-    "secret": process.env.JWT_KEY,
-    "refreshTokenSecret": process.env.REFRESH,
-    "tokenLife": 30
-  }
--- backend/database/data.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const config = require('../config')
-
-mongoose.connect(config.db, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-})
